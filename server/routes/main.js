07ca1631a46897aa79a9056cd9340fb61e0458f0
NAMGYEONGCHO <official2002mk@gmail.com>
2023-05-15 22:00:23 +0200

    added styles

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "ticket book",
+        description: "simple page"
+    }
+    res.render('index');
+})
+
+router.get('/about', (req, res) => {
+    res.render('about');
 })
 
 module.exports = router;
