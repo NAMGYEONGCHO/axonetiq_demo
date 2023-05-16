c554da187d091ed513551b5b62cc7657e2419b3e
NAMGYEONGCHO <official2002mk@gmail.com>
2023-05-16 06:56:12 +0200

    added current user button to display current user. ticket booking status would be changed along current user

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,60 @@
-//alert("hello");
+document.addEventListener('DOMContentLoaded', function(){
+
+    const allSeats = document.querySelectorAll('.seat');
+  
+    for (var i = 0; i < allSeats.length; i++) {
+      allSeats[i].addEventListener('click', function() {
+        // If the seat is available
+        if (this.classList.contains('available')) {
+          // Mark the seat as occupied
+          this.classList.remove('available');
+          this.classList.add('occupied');
+          this.textContent = 'Occupied';
+          // TODO: Add your booking logic here
+        } else if (this.classList.contains('occupied')) {
+          // Mark the seat as available
+          this.classList.remove('occupied');
+          this.classList.add('available');
+          this.textContent = 'Available';
+          // TODO: Add your logic here for when a seat is made available again
+        }
+      });
+    }
+
+    const allUsers = document.querySelectorAll('.user');
+  
+    for (var i = 0; i < allUsers.length; i++) {
+      allUsers[i].addEventListener('click', function() {
+        const userId = this.dataset.id;  // Get user id
+        switchUser(userId);
+      });
+    }
+    /* for (var i = 0; i < allUsers.length; i++) {
+      allUsers[i].addEventListener('click', function() {
+        const userId = this.dataset.id;  // Get user id
+        switchUser(userId);
+      });
+    } */
+
+});
+
+function switchUser(id) {
+  // Get the current user
+  const current = document.querySelector('.current');
+
+  // If the clicked user is already the current user, do nothing
+  if (current && current.id === `user-status-${id}`) {
+      return;
+  }
+
+  // Otherwise, remove 'current' class from the current user
+  if (current) {
+      current.classList.remove('current');
+      current.classList.add('hidden');
+  }
+
+  // And add 'current' class to the clicked user
+  const userCurrent = document.getElementById(`user-status-${id}`);
+  userCurrent.classList.remove('hidden');
+  userCurrent.classList.add('current');
+}
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,19 +1,85 @@
 const express = require('express');
 const router = express.Router();
-
+const Seat = require('../models/Seat')
+const User = require('../models/User')
 /**
  * 
  */
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
     const locals = {
         title: "ticket book",
         description: "simple page"
     }
-    res.render('index');
+
+    try {
+        const data = await Seat.find();
+        const users = await User.find();
+        res.render('index', { locals, data, users });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('An error occurred while retrieving data'); // Send an error response
+    }
 })
+
+
+
+/* app.put('/bookings/:id', (req, res) => {
+    // Get the id from the request parameters
+    const id = req.params.id;
+  
+    // Get the new status from the request body
+    const newStatus = req.body.status;
+  
+    // Connect to MongoDB and update the seat
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo.collection("tickets").updateOne({_id: new ObjectId(id)}, {$set: {status: newStatus}}, function(err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        db.close();
+      });
+    });
+  
+    res.send('Seat updated');
+  }); */
 
 router.get('/about', (req, res) => {
     res.render('about');
 })
 
 module.exports = router;
+
+/* function insertSeatData () {
+    Seat.insertMany([
+        {
+            number: 1,
+            status: false
+        },
+        {
+            number: 2,
+            status: false
+        },
+        {
+            number: 3,
+            status: false
+        }
+    ])
+}
+
+insertSeatData(); */
+
+/* function insertUserData () {
+    User.insertMany([
+        {
+            name: 'john',
+            tel: '23434512'
+        },
+        {
+            name: 'Doe',
+            tel: '11122234'
+        }
+    ])
+}
+
+insertUserData(); */
